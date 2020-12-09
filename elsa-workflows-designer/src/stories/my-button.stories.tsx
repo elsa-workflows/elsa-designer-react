db7a96877fe3602cf641baa7d78eccc903021c08
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-12-09 20:36:37 +0100

    Initial storybook scaffolding

---
+++ elsa-workflows-designer/src/stories/my-button.stories.tsx
@@ -0,0 +1,19 @@
+﻿import React, { ComponentProps } from 'react';
+import { Story } from '@storybook/react/types-6-0';
+
+import { MyButton } from '../components/my-button';
+
+// This default export determines where your story goes in the story list
+export default {
+    title: 'MyButton',
+    component: MyButton,
+};
+
+const Template: Story<ComponentProps<typeof MyButton>> = (args) => (
+    <MyButton label="Click me!" />
+);
+
+export const FirstStory = Template.bind({});
+FirstStory.args = {
+    /* the args you need here will depend on your component */
+};
---
+++ elsa-workflows-designer/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.tsx'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
